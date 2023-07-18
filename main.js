758b2d4a82cda5e0f14dde9bfd002d26e0cfaab2
paulkani <paulkani.s@golden-hippo.com>
2023-07-18 19:47:24 +0530

    pattern 3

---
+++ main.js
@@ -0,0 +1,40 @@
+var n=prompt("Enter n value");
+for(let i=1;i<=n;i++)
+{
+	for(let j=n;j>i;j--)
+	{
+		document.write("&nbsp;&nbsp;");
+	}
+	for(let j=1;j<=i;j++)
+	{
+		document.write(i);
+		 
+	}
+	for(let k=1;k<=i;k++)
+	{
+		document.write(i);
+		
+	}
+	 document.write("<br>");
+}
+
+
+
+// for(let i=1;i<=n;i++)
+// {
+	// for(let j=n;j>i;j--)
+	// {
+		// document.write("&nbsp;&nbsp;");
+	// }
+	// for(let j=1;j<=i;j++)
+	// {
+		// document.write(i);
+		 
+	// }
+	// for(let k=1;k<=i;k++)
+	// {
+		// document.write(i);
+		
+	// }
+	 // document.write("<br>");
+// }
